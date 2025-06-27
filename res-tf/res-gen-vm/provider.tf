d4f8168a7a6e1846fa4ae1761a03407f6482db7e
Pravin Tatti <pravin@rafay.co>
2025-06-28 01:06:18 +0530

    fix issues

---
+++ res-tf/res-gen-vm/provider.tf
@@ -0,0 +1,29 @@
+provider "kubernetes" {
+  alias       = "vcluster"
+  config_path = local_file.kubeconfig.filename
+}
+
+terraform {
+  required_providers {
+    rafay = {
+     source  = "RafaySystems/rafay"
+      version = "1.1.41"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.33.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+}
+
+provider "rafay" {
+  provider_config_file = var.rctl_config_path
+}
